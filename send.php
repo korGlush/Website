1f1e7707f7098ac3d2b04c83f67056fbaa195782
Glush <90596091+korGlush@users.noreply.github.com>
2022-02-15 11:10:52 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,39 +1,39 @@
 <?php
 
-$userName = $_POST['userName'];
-$userEmail = $_POST['userEmail'];
-$userPhone = $_POST['userPhone'];
+    $uesrName = $_POST['userName'];
+    $uesrEmail = $_POST['userEmail'];
+    $uesrPhone = $_POST['userPhone'];
 
-// Load Composer's autoloader
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+    //Load Composer's autoloader
+    require 'phpmailer/Exception.php';
+    require 'phpmailer/PHPMailer.php';
+    require 'phpmailer/SMTP.php';
 
-// Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer\PHPMailer\PHPMailer();
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
 
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'youremail@gmail.com';                     // SMTP username
-    $mail->Password   = 'password';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 465;                                    // TCP port to connect to
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'Почта';                     //SMTP username
+        $mail->Password   = 'Пароль почты';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set   `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('youremail@gmail.com');
-    $mail->addAddress('clientemail@yandex.ru');     // Add a recipient
+        //Recipients
+        $mail->setFrom('Почта', 'Алексей');
+        $mail->addAddress('Почта заказчика');     //Add a recipient
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Новая заявка с сайта';
-    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Новая заявка сайта';
+        $mail->Body    = "Имя пользователя: ${uername}, его телефон: ${userPhone}. Его почта: ${userEmail}";
 
-    $mail->send();
-    header('Location: thanks.html');
-} catch (Exception $e) {
-    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
-}
+        $mail->send();
+        header('Location: thanks.html');
+    } catch (Exception $e) {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
